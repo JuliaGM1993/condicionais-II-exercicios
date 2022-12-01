55f21533e216417384006a90c82adb12159921ae
JuliaGM1993 <juliagm1993@gmail.com>
2022-12-01 16:53:26 -0300

    Condicionais II exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,27 @@
-console.log("teste 1")
+let numero = prompt("Digite um número aqui")
+
+if(numero % 2 == 0){
+    if(numero % 3 === 0){
+console.log("O numero é divisivel por 2 ou 3")
+    }else if(numero % 2 === 0){
+        console.log("O número é divisível por 2")
+    }else if(numero % 3 === 0){
+        console.log("O núumero é divisível por 3")
+    }else{ 
+        console.log("O número não é divisível nem por 2 nem por 3")
+    }
+}
+
+let numero2 = prompt("Digite outro número aqui")
+
+if(numero2 % 2 === 0 && numero2 % 3 === 0 ){ 
+    console.log("O numero é divisível por 2 ou 3")
+    }else if(numero2 % 2 === 0){
+        console.log("O número é divisível por 2") 
+    }else if (numero2 % 3 === 0){
+        console.log("O número é divisível por 3")
+    }else{
+        console.log("O número não é divisível por 2 nem por 3")
+    }
+
 
